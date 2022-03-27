2e0e66ad963cfb94b7a22c2b775504667e8913e3
mohammedirsath <irshath700@gmail.com>
2022-03-27 17:06:04 +0530

    changed folder structure

--- components/Home/Hero.js
+++ components/Home/Hero.js
@@ -1,12 +1,9 @@
 import styles from "./hero.module.css";
-import classNames from "classnames/bind";
 
 function Hero() {
-	const cn = classNames.bind(styles);
-
 	return (
 		<>
-			<div className={cn("hero_container")}>
+			<div className={styles.hero_container}>
 				<h1>Wear your best moments!</h1>
 				<p>Handcrafted clothes for the Boys</p>
 				<button>Shop Now</button>
