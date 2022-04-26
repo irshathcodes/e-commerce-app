0d981c8012207f3cdd6583716b91014f32b8d89f
mohammedirsath <irshath700@gmail.com>
2022-04-26 23:18:33 +0530

    intro section done

---
+++ components/Home/Intro.jsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import styles from "./Intro.module.css";
+
+function Intro() {
+	return (
+		<>
+			<div className={styles.intro}>
+				{/* Image Container */}
+				<div className={styles.img_container}>
+					<div className={styles.img_wrapper + " " + styles.shirt_img}>
+						<Image
+							src="/intro_2.jfif"
+							className={styles.image}
+							layout={"fill"}
+							quality={100}
+						/>
+					</div>
+
+					<div className={styles.img_wrapper}>
+						<Image
+							src="/intro_1.jfif"
+							className={styles.image}
+							layout={"fill"}
+							quality={100}
+						/>
+					</div>
+					<div className={styles.intro_text}>
+						<h2>The Linen Collection</h2>
+						<p>
+							a modern fit with narrow shoulders and a tapered waist for a
+							fitted silhouette.
+						</p>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+}
+
+export default Intro;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Hero from "../components/Home/Hero";
+import Intro from "../components/Home/Intro";
 import ProductsOverview from "../components/Home/ProductsOverview";
 import About from "../components/Home/About";
 
@@ -6,6 +7,7 @@ export default function Home() {
 	return (
 		<>
 			<Hero />
+			<Intro />
 			<ProductsOverview />
 			<About />
 		</>
