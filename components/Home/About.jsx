791936049d16d4ec556fabc923b5d4e4a3e2edc5
mohammedirsath <irshath700@gmail.com>
2022-05-10 20:59:05 +0530

    working on newsletter

--- components/Home/About.jsx
+++ components/Home/About.jsx
@@ -1,22 +1,46 @@
 import styles from "./About.module.css";
+import Image from "next/image";
 
 function About() {
 	return (
-		<section className={styles.section}>
-			<h1>About Us</h1>
-			<div className="underline" style={{ paddingInline: "2rem" }}></div>
-			<p>
-				Welcome to <span>Cloth Zone</span>, your number one source for Men's
-				Clothing. We're dedicated to providing you the very best cloth material,
-				we handcraft all the products listed in our website to make sure you get
-				premium quality. Founded in 2022. Cloth Zone has come a long way from
-				its beginnings. We first started this company with passion for providing
-				sustainable premium quality products. We hope you enjoy our products as
-				much as we enjoy offering them to you. If you have any questions or
-				comments, please don't hesitate to contact us.
-				<span>Sincerely - Cloth Zone</span>
-			</p>
-		</section>
+		<>
+			<section className={styles.section}>
+				<div className={styles.image_container}>
+					<Image src="/about.svg" className={styles.image} layout="fill" />
+				</div>
+
+				<div className={styles.about_text}>
+					<h1>Who We Are</h1>
+					<p>
+						Hello Peeps, Welcome to Cloth Zone, your number one source for Men's
+						Clothing. We're dedicated to providing you the very best cloth
+						material, we handcraft all the products listed in our website to
+						make sure you get premium quality. Founded in 2022. Cloth Zone has
+						come a long way from its beginnings. We first started this company
+						with passion for providing sustainable premium quality products. We
+						hope you enjoy our products as much as we enjoy offering them to
+						you. If you have any questions or comments, please don't hesitate to
+						contact us
+					</p>
+				</div>
+			</section>
+			<NewsLetter />
+		</>
+	);
+}
+
+function NewsLetter() {
+	return (
+		<>
+			<div className={styles.newsletter}>
+				<h3>Join Our NewsLetter</h3>
+				<p>get the latest deals, discounts and more!</p>
+				<form className={styles.textbox_wrapper}>
+					<input type="email" placeholder="enter your email address" />
+					<button>Subscribe</button>
+				</form>
+			</div>
+		</>
 	);
 }
 
